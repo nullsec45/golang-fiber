9d4c6d4065895c1830c5a5f947bdde643408e915
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-21 21:05:36 +0700

    feat : ctx, request, route param.

--- fiber_test.go
+++ fiber_test.go
@@ -6,7 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http/httptest"
 	"io"
+	"net/http"
 )
+
+var app = fiber.New()
 
 func TestRoutingHelloWorld(t *testing.T) {
 	app := fiber.New()
@@ -22,4 +25,62 @@ func TestRoutingHelloWorld(t *testing.T) {
 	bytes, err := io.ReadAll(response.Body)
 	assert.Nil(t, err)
 	assert.Equal(t,"Hello World", string(bytes))
+}
+
+
+func TestCtx(t *testing.T) {
+	app.Get("/hello", func(ctx *fiber.Ctx) error {
+		name := ctx.Query("name","Guest")
+		return ctx.SendString("Hello "+name)
+	})
+
+	request := httptest.NewRequest("GET","/hello?name=Fajar", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t,"Hello Fajar", string(bytes))
+
+	request = httptest.NewRequest("GET","/hello", nil)
+	response, err = app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err = io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t,"Hello Guest", string(bytes))
+}
+
+func TestHttpRequest(t *testing.T) {
+	app.Get("/request", func(ctx *fiber.Ctx) error {
+		first := ctx.Get("firstname")
+		last := ctx.Cookies("lastname")
+		return ctx.SendString("Hello "+first+" "+last)
+	})
+
+	request := httptest.NewRequest("GET","/request", nil)
+	request.Header.Set("firstname","Rama")
+	request.AddCookie(&http.Cookie{Name:"lastname",Value:"Fajar"})
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t,"Hello Rama Fajar", string(bytes))
+}
+
+func TestRouteParameter(t *testing.T){
+	app.Get("/users/:userId/orders/:orderId", func(ctx *fiber.Ctx) error {
+		userId := ctx.Params("userId")
+		orderId := ctx.Params("orderId")
+		return ctx.SendString("Get Order "+orderId+" From User "+userId)
+	})
+
+	request := httptest.NewRequest("GET", "/users/1/orders/2", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+	bytes, err := io.ReadAll(response.Body)
+	assert.Equal(t, "Get Order 2 From User 1", string(bytes))
 }
